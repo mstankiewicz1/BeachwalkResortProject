7765c6fedc9ff207c1e2856b618cdddd96d5e4c4
Marek Stankiewicz <marek.stankiewicz@pvex.pl>
2021-05-01 21:05:48 +0200

    study 01.05.21

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 import Home from './pages/Home';
 
+import GlobalStyles from './components/globals/GlobalStyles';
+
 const App = () => {
   return (
     <>
+      <GlobalStyles/>
       <Home/>
     </>
   );
--- my-app/src/components/home/Header.js
+++ my-app/src/components/home/Header.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
+import Hero from '../globals/Hero';
+import aboutImg from '../../images/aboutBcg.jpeg';
+import homeImg from '../../images/homeBcg.jpeg';
+import Banner from '../globals/Banner';
+import {PrimaryBtn} from '../globals/Buttons';
+
 const Header = () => {
     return (
-        <div>
-            hello from header
-        </div>
+            <Hero img={homeImg}>
+                <Banner greeting="Welcome to " title="Beachwalk Resort" text="lorem ipsum">
+                    <PrimaryBtn t="1rem">View Details</PrimaryBtn>
+                    {/* <PrimaryBtn as="a" href="https://www.google.com">View Details</PrimaryBtn> */}
+                </Banner>
+            </Hero>
     )
 }
 
